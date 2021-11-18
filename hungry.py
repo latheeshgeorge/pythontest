10cf3f580cb97f5b21c21f63dce606df277cf148
latheeshgeorge <latheeshgeorge@gmail.com>
2021-11-18 14:15:39 +0530

    water

--- hungry.py
+++ hungry.py
@@ -2,3 +2,7 @@ hungry = input("are you hungry?")
 if hungry=="yes":
     print ("eat samosa")
     print ("eat piza")
+else:
+    thursty = input("are you thursty ? ")
+    if thursty=="yes":
+       print ("drink water")    
